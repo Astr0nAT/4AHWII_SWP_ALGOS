6f07ef804d4a107209470df0abfb822b8b38ef7b
Astr0nAT <astron.autnr@gmail.com>
2020-10-12 11:59:22 +0200

    summe rekursiv

--- Rekursion/src/Main.java
+++ Rekursion/src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(summeRekursiv(4));
+    }
+
+    private static int summeRekursiv(int n) {
+        if(n == 0){
+            return 1;
+        }
+
+        return summeRekursiv(n - 1) * n;
+    }
 }
